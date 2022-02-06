dc9128ea10b0c22236001b1ebc4c0df2b0a8172e
JaquelineRocio <jaquelineramosvargas@gmail.com>
2022-02-06 16:20:06 -0500

    background en colores gradiente y uso del efecto glass con backdrop-filter
    Validacion del input, que solo ingrese numeros y de 16 caracteres maximo

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import validator from './validator.js';
 
 //console.log(validator);
 let btnVerify=document.getElementById('btnVerify');
-const CardNumber=document.getElementById('CardNumber');
+const inpCardNumber=document.getElementById('inpCardNumber');
 let divVerified = document.getElementById('divVerified');
 
 btnVerify.addEventListener('click',()=>{
     //obtener el Numero de Tarjeta e insertarlo en un div del index.html
-    divVerified.innerHTML=validator.isValid(CardNumber.value);
+    divVerified.innerHTML=validator.isValid(inpCardNumber.value);
     
     validator.maskify();
 });
